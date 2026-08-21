577e0cc2737f8b269733e0e2890dcbd763e120f7
Victor Cervantes <vacervantes100@gmail.com>
2026-08-21 15:56:02 -0500

    first commit

---
+++ src/main/java/com/univ2026/practicas_omarchy/Practicas_omarchy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.univ2026.practicas_omarchy;
+
+/**
+ *
+ * @author victor
+ */
+public class Practicas_omarchy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
